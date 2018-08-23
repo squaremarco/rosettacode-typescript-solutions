49524f8ee5162ddc1eed380afd7ba7b970198c62
squaremarco <sq@iamsquare.it>
2018-08-23 13:06:12 +0200

    coded 99 bottle song generator

---
+++ src/99bottles.ts
@@ -0,0 +1,13 @@
+function bottles(): void {
+  for (let b = 99; b > 0; b--) {
+    let grammar= (n: number): string => (n === 1 ? 'bottle' : 'bottles');
+    console.log(
+      `\n${b} ${grammar(b)} of beer on the wall\n` +
+        `${b} ${grammar(b)} of beer\n` +
+        'Take one down, pass it around\n' +
+        `${b - 1} ${grammar(b - 1)} of beer on the wall\n`
+    );
+  }
+}
+
+bottles();
