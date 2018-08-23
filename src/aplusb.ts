aee11bf548dc474538dbc62b0507f83067649caa
squaremarco <sq@iamsquare.it>
2018-08-23 16:45:51 +0200

    coded A+B problem

---
+++ src/aplusb.ts
@@ -0,0 +1,17 @@
+import * as readline from 'readline-sync';
+import { ERROR, NOTIFY } from '../utils/colors';
+
+function stringSum(buf: string): string | number {
+  if (buf.match(/[^\d\s]/g)) {
+    return ERROR('\nInvalid input - only integers and whitespaces allowed.\n');
+  }
+  return NOTIFY(buf.match(/\d+/g).map(Number).reduce((acc, cur) => acc + cur));
+}
+
+while (true) {
+  let answer = readline.question(
+    'Insert integer numbers separated by a space:\n> '
+  );
+  if (answer === 'exit') process.exit();
+  console.log(stringSum(answer));
+}
