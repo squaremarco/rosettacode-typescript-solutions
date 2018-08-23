b14ab8d5bce693469b28f1eee1fb3e8d08061728
squaremarco <sq@iamsquare.it>
2018-08-23 13:06:01 +0200

    coded 24 game

---
+++ src/24game.ts
@@ -0,0 +1,53 @@
+import * as seval from 'safe-eval';
+import * as readline from 'readline-sync';
+import { isEqual } from 'lodash';
+import { NOTIFY, ERROR, WARN } from '../utils/colors';
+
+function twentyFour(randNumbers: Array<number>, input: string): void {
+  const strippedInput = input.replace(/\s/g, '');
+
+  if (!strippedInput.match(/[-+*\/)(\d]/g)) {
+    console.log(
+      ERROR(
+        '\nInvalid input - only numbers, whitespaces and + - * / allowed.\n'
+      )
+    );
+    return;
+  }
+
+  const inputNumbers = strippedInput
+    .replace(/\D/g, '')
+    .split('')
+    .map(el => parseInt(el))
+    .sort();
+
+  if (!isEqual(randNumbers, inputNumbers)) {
+    console.log(ERROR('\nInvalid numbers - only use generated numbers.\n'));
+    return;
+  }
+
+  try {
+    if (seval(strippedInput) !== 24) {
+      console.log(WARN(`\n${strippedInput} !== 24\n`));
+      return;
+    } else {
+      console.log(NOTIFY(`\n${strippedInput} === 24 - CORRECT!`));
+      process.exit();
+    }
+  } catch (e) {
+    console.log(ERROR(`\n${e.toString()}\n`));
+    return;
+  }
+}
+
+const randNumbers: Array<number> = Array.from({ length: 4 }, () =>
+  Math.ceil(Math.random() * 9)
+).sort();
+
+while (true) {
+  let answer = readline.question(
+    `These are the generated numbers: ${randNumbers}, insert expression:\n> `
+  );
+  if (answer === 'exit') process.exit();
+  twentyFour(randNumbers, answer);
+}
