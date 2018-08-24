6028f5f3d20632095362a9dc3172ab815137d369
squaremarco <sq@iamsquare.it>
2018-08-24 12:32:50 +0200

    coded babbage problem

---
+++ src/babbage.ts
@@ -0,0 +1,22 @@
+const BABBAGE: number = 269696;
+function babbage(): Array<number> {
+  let n: number = Math.trunc(Math.sqrt(BABBAGE));
+
+  while (Math.pow(n, 2) % 1000000 !== BABBAGE) {
+    n++;
+  }
+  return [n, n * n];
+}
+
+function babbageList(testN: number): Array<Array<number>> {
+  const res = Array.from({ length: testN }, (v, i) => i * 1000000 + BABBAGE)
+    .filter(n => {
+      const root = Math.sqrt(n);
+      return root === Math.floor(root);
+    })
+    .map(n => [Math.sqrt(n), n]);
+  return res;
+}
+
+console.log(babbage());
+console.log(babbageList(10E3)); // 10^5 numbers ending in BABBAGE
