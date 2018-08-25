7958d657656c22118c4fc5f6b673523dee6052d2
squaremarco <sq@iamsquare.it>
2018-08-25 10:37:23 +0200

    added iterative functions for binary<->decimal conversion

--- src/binaryDigits.ts
+++ src/binaryDigits.ts
@@ -1,9 +1,32 @@
-import { Justify, justifyString } from '../utils/justify';
+function binToDecimalIterative(b: string): number {
+  if (b.match(/[^01]/g)) return -1;
+  let result: number = 0;
+  for (let i = 0; i < b.length; i++) {
+    result = result + parseInt(b[i]) * Math.pow(2, b.length - i - 1);
+  }
+  return result;
+}
+
+function binDigitsIterative(n: number): string {
+  let result: Array<string> = [];
+  let i = n;
+  do {
+    result.unshift(`${i % 2}`);
+    i = Math.trunc(i / 2);
+  } while (i !== 0);
+  return result.join('');
+}
+
+function binToDecimal(b: string): number {
+  if (b.match(/[^01]/g)) return -1;
+  return parseInt(b, 2);
+}
 
 function binDigits(n: number): string {
   return n.toString(2);
 }
 
 for (let i = 0; i < Math.pow(2, 5); i++) {
-  console.log(justifyString(binDigits(i), 5, Justify.RIGHT));
+  let string: string = binDigits(i);
+  console.log(`${binToDecimal(string)} <-> ${string}`);
 }
