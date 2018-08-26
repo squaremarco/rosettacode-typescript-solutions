ca639483d21b3e3af53154c9d4f06a55c7ecfaa2
squaremarco <sq@iamsquare.it>
2018-08-26 20:47:20 +0200

    merged agmPI.ts and agm.ts in a single file

--- src/agm.ts
+++ src/agm.ts
@@ -1,10 +1,28 @@
-import { config, format, add, multiply, subtract, sqrt } from 'mathjs'; //operators
+import { config, add, multiply, subtract, divide, sqrt, pow } from 'mathjs'; //operators
 import { BigNumber, bignumber } from 'mathjs'; //types
 
 const precision: number = 64;
 config({
   precision
 });
+
+export function agmPI(tolerance: BigNumber = bignumber(`10E-${precision}`)): BigNumber {
+  let a0: BigNumber = bignumber(1);
+  let g0: BigNumber = bignumber(1 / sqrt(2));
+  let c0: BigNumber = <BigNumber>multiply(bignumber(0.5), subtract(a0, g0));
+  let agm0: BigNumber = agm(a0, g0, tolerance);
+
+  let [an, gn, cn] = [a0, g0, c0];
+  let sum: BigNumber = <BigNumber>multiply(pow(bignumber(2), 2), pow(c0, 2));
+
+  for (let i = 2; (<BigNumber>subtract(an, gn)).gt(tolerance); i++) {
+    [an, gn] = [<BigNumber>multiply(bignumber(0.5), add(an, gn)), <BigNumber>sqrt(<BigNumber>multiply(an, gn))];
+    cn = <BigNumber>multiply(bignumber(0.5), subtract(an, gn));
+    sum = <BigNumber>add(sum, multiply(pow(bignumber(2), i + 1), pow(cn, 2)));
+  }
+
+  return <BigNumber>divide(multiply(bignumber(4), pow(agm0, 2)), subtract(bignumber(1), sum));
+}
 
 export default function agm(a: BigNumber, g: BigNumber, tolerance: BigNumber = bignumber(`10E-${precision}`)): BigNumber {
   if (!a.isPos() || !g.isPos() || a.lt(g)) return bignumber(-1);
@@ -15,6 +33,8 @@ export default function agm(a: BigNumber, g: BigNumber, tolerance: BigNumber = b
   }
   return an;
 }
+
+
 
 //console.log(format(agm(bignumber(1), bignumber(1 / sqrt(2)))));
 //console.log(format(agm(bignumber(24), bignumber(6))));
