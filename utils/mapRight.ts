5278d43edac1d6a58893686f4be4e761a3824cd7
squaremarco <sq@iamsquare.it>
2018-08-26 15:12:19 +0200

    added this arg binding to mapRight

--- utils/mapRight.ts
+++ utils/mapRight.ts
@@ -1,4 +1,9 @@
-export default function mapRight<T, U>(array: Array<T>, callbackfn: (value: T, index: number, array: Array<T>) => U): Array<U> {
+export default function mapRight<T, U>(
+  array: Array<T>,
+  callbackfn: (value: T, index: number, array: Array<T>) => U,
+  thisArg?: any
+): Array<U> {
+  if(thisArg) callbackfn = callbackfn.bind(thisArg);
   return array.reduceRight(
     (accumulator: Array<U>, current: T, index: number, arr: Array<T>) => {
       return (accumulator = accumulator.concat(callbackfn(current, index, arr)));
