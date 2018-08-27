4b09a4854db79ff73030f56d7fb0cb01debbc1b4
squaremarco <sq@iamsquare.it>
2018-08-27 13:40:52 +0200

    refactored averages variable names

--- src/averages.ts
+++ src/averages.ts
@@ -1,64 +1,91 @@
-function arithmeticMean(n: Array<number>): number {
-  if (!n.length) return NaN;
-  return n.reduce((a, c) => a + c, 0) / n.length;
+function arithmeticMean(arr: Array<number>): number {
+  if (!arr.length) return NaN;
+  return arr.reduce((a, c) => a + c, 0) / arr.length;
 }
 
-function geometricMean(n: Array<number>): number {
-  if (!n.length) return NaN;
-  return n.reduce((a, c) => a * c, 1) ** (1 / n.length);
+function geometricMean(arr: Array<number>): number {
+  if (!arr.length) return NaN;
+  return arr.reduce((a, c) => a * c, 1) ** (1 / arr.length);
 }
 
-function harmonicMean(n: Array<number>): number {
-  if (!n.length) return NaN;
-  return n.length / n.reduce((a, c) => a + 1 / c, 0);
+function harmonicMean(arr: Array<number>): number {
+  if (!arr.length) return NaN;
+  return arr.length / arr.reduce((a, c) => a + 1 / c, 0);
 }
 
 //root mean square
-function rms(n: Array<number>): number {
-  if (!n.length) return NaN;
-  return (n.reduce((a, c) => a ** 2 + c ** 2, 0) / n.length) ** (1 / 2);
+function rms(arr: Array<number>): number {
+  if (!arr.length) return NaN;
+  return (arr.reduce((a, c) => a ** 2 + c ** 2, 0) / arr.length) ** (1 / 2);
 }
 
 //mean, variance, stdev
-function mvs(n: Array<number>): { m: number; v: number; s: number } {
-  if (!n.length) return { m: NaN, v: NaN, s: NaN };
-  let m: number = arithmeticMean(n);
-  let v: number = n.reduce((a, c) => a + (c - m) ** 2, 0) / n.length;
-  let s: number = v ** (1 / 2);
+function mvs(arr: Array<number>): { m: number; v: number; s: number } {
+  if (!arr.length) return { m: NaN, v: NaN, s: NaN };
+  let m: number = arithmeticMean(arr);
+  let v: number = arr.reduce((a, c) => a + (c - m) ** 2, 0) / arr.length;
+  let s: number = Math.sqrt(v);
   return { m, v, s };
 }
 
 //sampled mvs
-function smvs(n: Array<number>): { m: number; v: number; s: number } {
-  if (!n.length) return { m: NaN, v: NaN, s: NaN };
-  let m: number = arithmeticMean(n);
-  let v: number = n.reduce((a, c) => a + (c - m) ** 2, 0) / (n.length - 1);
-  let s: number = v ** (1 / 2);
+function smvs(arr: Array<number>): { m: number; v: number; s: number } {
+  if (!arr.length) return { m: NaN, v: NaN, s: NaN };
+  let m: number = arithmeticMean(arr);
+  let v: number = arr.reduce((a, c) => a + (c - m) ** 2, 0) / (arr.length - 1);
+  let s: number = Math.sqrt(v);
   return { m, v, s };
 }
 
-//simple moving average
-function smv(period: number): (value: number) => number {
-  if (period <= 0) throw new Error('NotPositivePeriod');
-  let P: number = period;
+//cumulative mvs
+function cmvs(): (n?: number) => { m: number; v: number; s: number } {
+  let count: number = 0;
   let sum: number = 0;
-  let avg: number = 0;
+  let sumSquares: number = 0;
+  let m: number = 0;
+  let v: number = 0;
+  let s: number = 0;
+
+  return (n?: number) => {
+    if (typeof n !== 'undefined') {
+      sum += n;
+      sumSquares += n * n;
+      count++;
+      m = sum / count;
+      v = sumSquares / count - m * m;
+      s = Math.sqrt(v);
+    }
+
+    return { m, v, s };
+  };
+}
+
+//simple moving average
+function smv(period: number): (value?: number) => { m: number; stream: Array<number> } {
+  if (period < 1) throw new Error('NonPositivePeriod');
+  if (period % 1 !== 0) throw new Error('NonIntegerPeriod');
+  
+  const P: number = period;
+  let sum: number = 0;
+  let m: number = 0;
   let stream: Array<number> = [];
-  return (n: number): number => {
+
+  return (n?: number): { m: number; stream: Array<number> } => {
+    if (typeof n === 'undefined') return { m, stream };
     stream.push(n);
     sum += n;
     if (stream.length > P) {
       sum -= stream.shift();
-      avg = sum / P;
+      m = sum / P;
     } else {
-      avg = sum / stream.length;
+      m = sum / stream.length;
     }
-    return avg;
+    return { m, stream };
   };
 }
 
-function mode(n: Array<number>): number | Array<number> {
-  let countIterable: IterableIterator<[number, number]> = n
+function mode(arr: Array<number>): number | Array<number> {
+  const countIterable: IterableIterator<[number, number]> = arr
     .reduce((a, c) => {
       if (!a.has(c)) return a.set(c, 1);
       return a.set(c, a.get(c) + 1);
@@ -67,6 +94,7 @@ function mode(n: Array<number>): number | Array<number> {
 
   let maxCount: number = -1;
   let result: Array<number> = [];
+
   for (let el of countIterable) {
     if (el[1] === maxCount) {
       result.push(el[0]);
@@ -77,12 +105,13 @@ function mode(n: Array<number>): number | Array<number> {
       result.push(el[0]);
     }
   }
+
   return result.length === 1 ? result[0] : result;
 }
 
-function median(n: Array<number>): number {
-  let arr = [...n].sort();
-  let index = arr.length / 2;
-  if (n.length % 2 === 0) return (n[index] + n[index - 1]) / 2;
-  return n[Math.floor(index)];
+function median(arr: Array<number>): number {
+  let arrCopy = [...arr].sort();
+  let index = arrCopy.length / 2;
+  if (arrCopy.length % 2 === 0) return (arrCopy[index] + arrCopy[index - 1]) / 2;
+  return arrCopy[Math.floor(index)];
 }
