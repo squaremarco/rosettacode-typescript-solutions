a503a3a71bed42ff7bc24ddf1d695d628e266b82
squaremarco <sq@iamsquare.it>
2018-08-27 13:41:42 +0200

    fixed mapRight thisarg typechecking

--- utils/mapRight.ts
+++ utils/mapRight.ts
@@ -3,11 +3,8 @@ export default function mapRight<T, U>(
   callbackfn: (value: T, index: number, array: Array<T>) => U,
   thisArg?: any
 ): Array<U> {
-  if(thisArg) callbackfn = callbackfn.bind(thisArg);
-  return array.reduceRight(
-    (accumulator: Array<U>, current: T, index: number, arr: Array<T>) => {
-      return (accumulator = accumulator.concat(callbackfn(current, index, arr)));
-    },
-    <Array<U>>[]
-  );
+  if (typeof thisArg !== 'undefined') callbackfn = callbackfn.bind(thisArg);
+  return array.reduceRight((accumulator: Array<U>, current: T, index: number, arr: Array<T>) => {
+    return (accumulator = accumulator.concat(callbackfn(current, index, arr)));
+  }, []);
 }
