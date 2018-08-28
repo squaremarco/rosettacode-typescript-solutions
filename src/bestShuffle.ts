03b4e7cac7fcc6cc3baab746b4d0d739f7dcad02
squaremarco <sq@iamsquare.it>
2018-08-28 23:23:39 +0200

    refactored bestshuffle code

--- src/bestShuffle.ts
+++ src/bestShuffle.ts
@@ -2,17 +2,13 @@ import { random } from 'lodash';
 import shuffle from '../utils/shuffle';
 
 function bestShuffle(input: Array<number>): { shuffledInput: Array<number>; overlap: number } {
-  let shuffledInput: Array<number> = [];
-  let foundShuffle = false;
-  let duplicates: { [key: number]: any } = {};
-
-  input.forEach((v) => {
-    if (!duplicates[v]) {
-      duplicates[v] = 1;
-    } else {
-      duplicates[v]++;
-    }
-  });
+  let duplicates: { [key: number]: any } = input.reduce(
+    (a, c) => {
+      a[c] = !a[c] ? 1 : a[c] + 1;
+      return a;
+    },
+    <{ [key: number]: any }>{}
+  );
 
   let maxDuplication: number = -1;
   for (let d in duplicates) {
@@ -22,15 +18,18 @@ function bestShuffle(input: Array<number>): { shuffledInput: Array<number>; over
   let maxOverlap: number = 2 * (maxDuplication - input.length / 2);
   maxOverlap = maxOverlap > 0 ? maxOverlap : 0;
 
+  let shuffledInput: Array<number> = [];
   let overlap: number;
-  while (!foundShuffle) {
+  
+  while(true) {
     overlap = 0;
     shuffledInput = shuffle(input);
-    for (let i = 0; i < input.length; i++) {
-      if (input[i] === shuffledInput[i]) overlap++;
+    for (let i = 0; overlap <= maxOverlap && i < shuffledInput.length ; i++){
+      if(shuffledInput[i] === input[i]) overlap++;
     }
-    foundShuffle = overlap <= maxOverlap;
+    if (overlap <= maxOverlap) break;
   }
+
   return { shuffledInput, overlap };
 }
 
