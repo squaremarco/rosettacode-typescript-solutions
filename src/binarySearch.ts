2f63ad5a7a14a36150fd0b04846d0f92ad337c08
squaremarco <sq@iamsquare.it>
2018-08-29 16:59:36 +0200

    Coded binarySearch

---
+++ src/binarySearch.ts
@@ -0,0 +1,25 @@
+function recursiveBinarySearch(
+  array: Array<number>,
+  value: number,
+  low: number = 0,
+  high: number = array.length - 1,
+  findInsertionIndex: boolean = false
+): number {
+  if (high < low) return findInsertionIndex ? low : -1;
+  const mid: number = low + Math.floor((high - low) / 2);
+  if (array[mid] > value) return recursiveBinarySearch(array, value, low, mid - 1);
+  if (array[mid] < value) return recursiveBinarySearch(array, value, mid + 1, high);
+  return mid;
+}
+
+function iterativeBinarySearch(array: Array<number>, value: number, findInsertionIndex: boolean = false): number {
+  let low = 0;
+  let high = array.length - 1;
+  while (low <= high) {
+    let mid = low + Math.floor((high - low) / 2);
+    if (array[mid] === value) return mid;
+    if (array[mid] > value) high = mid - 1;
+    else low = mid + 1;
+  }
+  return findInsertionIndex ? low : -1;
+}
